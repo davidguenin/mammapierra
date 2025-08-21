cf913e979ecf9beda52ea985a4805d989cfd3ae1
davidguenin <guenin.david@gmail.com>
2025-08-21 14:11:00 +0200

    WIP

--- assets/infinite-scroll.js
+++ assets/infinite-scroll.js
@@ -1,40 +1,81 @@
 class InfiniteScroll {
   constructor() {
     this.button = document.querySelector('.infinite-scroll-button');
-    if (!this.button) return;
+    this.trigger = document.querySelector('.infinite-scroll-trigger');
+
+    if (this.button) {
+      this.button.addEventListener('click', this.loadMoreProducts.bind(this));
+    } else if (this.trigger) {
+      this.observer = new IntersectionObserver(this.handleIntersect.bind(this), {
+        rootMargin: '0px 0px 200px 0px',
+      });
+      this.observer.observe(this.trigger);
+    } else {
+      return;
+    }
 
     this.loading = document.querySelector('.infinite-scroll-loading');
     this.productGrid = document.querySelector('#product-grid');
+  }
 
-    this.button.addEventListener('click', this.loadMoreProducts.bind(this));
+  handleIntersect(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        this.loadMoreProducts();
+      }
+    });
   }
 
   loadMoreProducts() {
-    const nextUrl = this.button.dataset.nextUrl;
-    if (!nextUrl) {
-      this.button.remove();
+    let url;
+    if (this.button) {
+      url = this.button.dataset.nextUrl;
+    } else if (this.trigger) {
+      url = this.trigger.dataset.nextUrl;
+    }
+
+    if (!url) {
+      if (this.button) this.button.remove();
+      if (this.trigger) {
+        this.observer.unobserve(this.trigger);
+        this.trigger.remove();
+      }
       return;
     }
 
     this.showLoading();
-    this.button.style.display = 'none';
+    if (this.button) this.button.style.display = 'none';
 
-    fetch(nextUrl)
+    fetch(url)
       .then((response) => response.text())
       .then((text) => {
         const html = new DOMParser().parseFromString(text, 'text/html');
         const newProducts = html.querySelectorAll('#product-grid > .grid__item');
+        const newTrigger = html.querySelector('.infinite-scroll-trigger');
         const newButton = html.querySelector('.infinite-scroll-button');
 
         newProducts.forEach((product) => {
           this.productGrid.appendChild(product);
         });
 
-        if (newButton && newButton.dataset.nextUrl) {
-          this.button.dataset.nextUrl = newButton.dataset.nextUrl;
-          this.button.style.display = 'block';
-        } else {
-          this.button.remove();
+        if (this.button) {
+          if (newButton && newButton.dataset.nextUrl) {
+            this.button.dataset.nextUrl = newButton.dataset.nextUrl;
+            this.button.style.display = 'block';
+          } else {
+            this.button.remove();
+          }
+        }
+
+        if (this.trigger) {
+          if (newTrigger && newTrigger.dataset.nextUrl) {
+            this.trigger.dataset.nextUrl = newTrigger.dataset.nextUrl;
+          } else {
+            if (this.trigger) {
+              this.observer.unobserve(this.trigger);
+              this.trigger.remove();
+            }
+          }
         }
 
         this.hideLoading();
@@ -42,7 +83,7 @@ class InfiniteScroll {
       .catch((error) => {
         console.error('Error loading more products:', error);
         this.hideLoading();
-        this.button.style.display = 'block';
+        if (this.button) this.button.style.display = 'block';
       });
   }
 
